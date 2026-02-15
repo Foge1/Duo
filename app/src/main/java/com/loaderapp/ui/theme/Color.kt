2669cd390dd842c31aed280d692120f7e4756083
Bedniy <fedukinskiy@gmail.com>
2026-02-15 19:39:37 +0300

    jlh

---
+++ app/src/main/java/com/loaderapp/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.loaderapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Изумрудная светлая тема
+val Emerald80 = Color(0xFF80E9C5)
+val EmeraldGrey80 = Color(0xFFB2D8CE)
+val Mint80 = Color(0xFFA8E6CF)
+
+// Изумрудная темная тема
+val Emerald40 = Color(0xFF2D9B75)
+val EmeraldGrey40 = Color(0xFF4A7C6B)
+val Mint40 = Color(0xFF3D8B6F)
